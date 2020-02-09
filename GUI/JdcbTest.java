2b9bfaa113e3ba96a075d4c5851e8f9b11eae655
SmallPretz <damianjakubkaminski@gmail.com>
2020-02-09 16:34:45 +0100

    dodanie opisów

--- GUI/JdcbTest.java
+++ GUI/JdcbTest.java
@@ -28,17 +28,17 @@ public class JdcbTest {
         while(np<=9) {
 
         Scanner keyboardinput = new Scanner(System.in);
-
+            //pytanie
             for (int j = 0; j < pytania.size(); j++) {
                 if (pytania.get(j).getQuestionId() == np)
                     System.out.println(pytania.get(j));
             }
-
+            //odpowiedzi
             for (int i = 0; i < odpowiedzi.size(); i++) {
                 if (odpowiedzi.get(i).getIdQuestion() == np)
                     System.out.println(odpowiedzi.get(i));
             }
-
+            //poprawna odpowiedzi nie widziana dla usera
             for (int q = 0; q < czyPoprawne.size(); q++) {
                 if (czyPoprawne.get(q).getIdQuestion() == np){
                     if(czyPoprawne.get(q).getIsCorrect() == Boolean.TRUE) {
@@ -48,6 +48,7 @@ public class JdcbTest {
                 }
 
             }
+            //zliczanie punktów
             String answer = keyboardinput.nextLine();
             if(answer.equals(poprawnaOdpowiedz)){
                 score ++;
@@ -55,7 +56,7 @@ public class JdcbTest {
 
             np = np + 1;
         }
-        //
+        //podsumowanie
         System.out.println("You got " + score + "/" + (np - 1));
 
 
